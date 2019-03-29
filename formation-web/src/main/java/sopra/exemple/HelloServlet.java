ae0973f4155bc6f79a6782001565b5134349e061
DUBOURG <jasondubourg@gmail.com>
2019-03-29 15:18:36 +0100

    Jason

--- formation-web/src/main/java/sopra/exemple/HelloServlet.java
+++ formation-web/src/main/java/sopra/exemple/HelloServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class HelloServlet extends HttpServlet {
+public class HelloServlet extends HttpServlet { // Classe java accessible par une servelet via son url
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
