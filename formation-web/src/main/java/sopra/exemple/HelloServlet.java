ba3ec7931fc4e1ebfb11ee85668965d3a0385604
eric <e.sultan@ajc-ingenierie.fr>
2019-03-29 16:15:00 +0100

    cookie

--- formation-web/src/main/java/sopra/exemple/HelloServlet.java
+++ formation-web/src/main/java/sopra/exemple/HelloServlet.java
@@ -3,6 +3,7 @@ package sopra.exemple;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,19 +11,42 @@ import javax.servlet.http.HttpServletResponse;
 public class HelloServlet extends HttpServlet { // Classe java accessible par une servelet via son url
 
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
 		String nom = request.getParameter("nom");
-		
+
+		if (nom != null && !nom.isEmpty()) {
+			Cookie cookie = new Cookie("cookieNom", nom);
+			cookie.setMaxAge(20);
+
+			response.addCookie(cookie);
+		} else {
+			Cookie[] cookies = request.getCookies();
+
+			if (cookies != null) {
+				for (Cookie cookie : cookies) {
+					if (cookie.getName().equals("cookieNom")) {
+						nom = cookie.getValue();
+						break;
+					}
+				}
+			}
+		}
+
 		response.setContentType("text/html");
 		response.getWriter().write("<html>");
 		response.getWriter().write("<head>");
 		response.getWriter().write("<title>Ma première servlet</title>");
 		response.getWriter().write("</head>");
 		response.getWriter().write("<body>");
-		response.getWriter().write("<h1>Hello</h1>");
+		if (nom != null && !nom.isEmpty()) {
+			response.getWriter().write("<h1>Hello " + nom + "</h1>");
+		} else {
+			response.getWriter().write("<h1>Hello le monde</h1>");
+		}
 		response.getWriter().write("</body>");
-		
+
 		response.getWriter().write("</html>");
 	}
 
@@ -30,7 +54,5 @@ public class HelloServlet extends HttpServlet { // Classe java accessible par un
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		doGet(req, resp);
 	}
-	
-	
 
 }
